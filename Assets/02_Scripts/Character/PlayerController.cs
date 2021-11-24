151c46871b19ac57703f91a51345b4f26a05ef9e
재원 이 <jaewon990905@gmail.com>
2021-11-24 19:05:52 +0900

    Basic monster FSM add LJW

--- Assets/02_Scripts/Character/PlayerController.cs
+++ Assets/02_Scripts/Character/PlayerController.cs
@@ -1,58 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
-    private PlayerMovement playerMovement;
-    [SerializeField] private CameraController cameraController;
-    [SerializeField] private Animator animator;
+    [SerializeField] private float moveSpeed = 5.0f;
     
-    void Awake()
-    {
-        playerMovement = GetComponent<PlayerMovement>();
-    }
-
+    [SerializeField] private Transform characterBody;
+    [SerializeField] private Transform cameraArm;
+    [SerializeField] private Animator animator;
+    [SerializeField] private CharacterController characterController;
+    
     void Update()
     {
         KeyInput();
         MouseInput();
         Attack();
         
-        
     }
 
     void KeyInput()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float z = Input.GetAxisRaw("Vertical");
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        if (x * x + z * z != 0)
+        if (moveInput.magnitude != 0)
         {
             animator.SetBool("isWalk", true);
             if (Input.GetKeyDown(KeyCode.LeftShift))
             {
                 animator.SetBool("isRun", true);
-                playerMovement.ChangeMoveSpeed(5.0f);
+                moveSpeed = 5.0f;
             }
         }
         else
         {
             animator.SetBool("isWalk", false);
             animator.SetBool("isRun", false);
-            playerMovement.ChangeMoveSpeed(3.0f);
+            moveSpeed = 3.0f;
         }
 
-        playerMovement.MoveTo(new Vector3(x, 0, z));
+        Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+        Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+        Vector3 moveDirection = lookForward * moveInput.y + lookRight * moveInput.x;
+
+        characterBody.forward = lookForward;
+        
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
     }
 
     void MouseInput()
     {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        Vector3 camAngle = cameraArm.rotation.eulerAngles;
+
+        float a = camAngle.x - mouseDelta.y;
+
+        if (a < 180f)
+        {
+            a = Mathf.Clamp(a, -1f, 70f);
+        }
+        else
+        {
+            a = Mathf.Clamp(a, 335f, 361f);
+
+        }
         
-        cameraController.RotateTo(mouseX, mouseY);
-        playerMovement.RotateTo(mouseX);
+        cameraArm.rotation = Quaternion.Euler(camAngle.x - mouseDelta.y, camAngle.y + mouseDelta.x, camAngle.z);
     }
 
     void Attack()
