257fe03c6fac57f1c33780e14ed387781eeb51f7
재원 이 <jaewon990905@gmail.com>
2021-11-25 06:26:20 +0900

    attack, move, die state add

---
+++ Assets/02_Scripts/Monster/FSM_State.cs
@@ -0,0 +1,8 @@
+abstract public class FSM_State<T>
+{
+    abstract public void EnterState(T _Monster);
+
+    abstract public void UpdateState(T _Monster);
+
+    abstract public void ExitState(T _Monster);
+}
