39639cf116c6672ed323d3d92772af5cebbc9d2a
재원 이 <jaewon990905@gmail.com>
2021-11-26 01:40:54 +0900

    map asset add PSJ

--- Assets/02_Scripts/Enemy/FSM_State.cs
+++
@@ -1,8 +0,0 @@
-abstract public class FSM_State<T>
-{
-    abstract public void EnterState(T _Monster);
-
-    abstract public void UpdateState(T _Monster);
-
-    abstract public void ExitState(T _Monster);
-}
